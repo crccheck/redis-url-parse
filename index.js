c6b73166bb5cd94233ad0d5a330dc924471e01c6
crccheck <c@crccheck.com>
2017-04-06 15:08:51 -0500

    add some flow type hints as documentation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+// @flow
 const urlParse = require('url').parse;
 
-module.exports = (url) => {
+/*:: type RedisConfig = {
+  host: string,
+  port: number,
+  database: string,
+  password: ?string
+} */
+
+module.exports = (url/*: string */)/*: RedisConfig */ => {
   const redisConfig = urlParse(url);
   return {
     host: redisConfig.hostname || 'localhost',
-    port: redisConfig.port || 6379,
+    port: Number(redisConfig.port || 6379),
     database: (redisConfig.pathname || '/0').substr(1) || '0',
     password: redisConfig.auth && redisConfig.auth.substr(1)
   }
